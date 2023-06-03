eff7747351de5b3790e02f909d5029a64e56a9b3
Luiz Henrique Santos <luiz.santos85@gmail.com>
2023-06-03 19:39:43 -0300

    estruturas iniciais de tenant e user

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    
+    /**
+     *  RELACIONAMENTOS
+     */
+    public function tenant()
+    {
+        return $this->belongsTo(Tenant::class, 'tenant_id','id');
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,13 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->unsignedBigInteger('tenant_id');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('tenant_id')->references('id')->on('tenants')->onDelete('cascade');
         });
     }
 
